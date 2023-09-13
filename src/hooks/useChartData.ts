c34e49bdc8e5d0bd3b510a9616f60ee474001034
Minsoek96 <fe.minseok96@gmail.com>
2023-09-13 15:18:04 +0900

    feat : chartData를 받아오기 위한 커스텀훅 추가

---
+++ src/hooks/useChartData.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+interface ChartData {
+  time: string;
+  id: string;
+  value_area: number;
+  value_bar: number;
+}
+
+export const useChartData = () => {
+  const [chartData, setChartData] = useState<ChartData[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/");
+        const data = response.data;
+        const newData = Object.keys(data).map((key) => ({
+          time: key,
+          ...data[key],
+        }));
+        setChartData(newData);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { chartData, isLoading };
+};
